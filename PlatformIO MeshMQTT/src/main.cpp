1eff42e03fa89178b3788105966a37c03c8d4573
Jimmy Su <jmysu@users.noreply.github.com>
2018-06-29 15:50:36 +0800

    Add files via upload

---
+++ PlatformIO MeshMQTT/src/main.cpp
@@ -0,0 +1,104 @@
+#include <Arduino.h>
+#include <painlessMesh.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
+
+#define   MESH_PREFIX     "MyMesh"
+#define   MESH_PASSWORD   "12345678"
+#define   MESH_PORT       5555
+
+#define   STATION_SSID     "BreezeHill_50"
+#define   STATION_PASSWORD "0932902190"
+
+#define HOSTNAME "MeshMQTT"
+
+// Prototypes
+void receivedCallback( const uint32_t &from, const String &msg );
+void mqttCallback(char* topic, byte* payload, unsigned int length);
+
+IPAddress getlocalIP();
+
+IPAddress myIP(0,0,0,0);
+IPAddress mqttBroker(192, 168, 0, 103);
+
+painlessMesh  mesh;
+WiFiClient wifiClient;
+PubSubClient mqttClient(mqttBroker, 3883, mqttCallback, wifiClient);
+
+void setup() {
+  Serial.begin(115200);
+
+  mesh.setDebugMsgTypes( ERROR | STARTUP | CONNECTION );  // set before init() so that you can see startup messages
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+
+  // Channel set to 6. Make sure to use the same channel for your mesh and for you other
+  // network (STATION_SSID)
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6 );
+  mesh.onReceive(&receivedCallback);
+
+  mesh.stationManual(STATION_SSID, STATION_PASSWORD);
+  mesh.setHostname(HOSTNAME);
+}
+
+void loop() {
+  mesh.update();
+  mqttClient.loop();
+
+  if(myIP != getlocalIP()){
+    myIP = getlocalIP();
+    Serial.println("My IP is " + myIP.toString());
+    String msg = "My Node is ";
+    msg += mesh.getNodeId();
+    Serial.println(msg);
+
+    if (mqttClient.connect("MyMeshClient")) {
+      mqttClient.publish("MyMesh/from/gateway","Ready!");
+      mqttClient.subscribe("MyMesh/to/#");
+    }
+  }
+}
+
+void receivedCallback( const uint32_t &from, const String &msg ) {
+  Serial.printf("bridge: Received from $%03u msg=%s\n", from%1000, msg.c_str());
+  String sFrom = String(from);
+  String topic = "MyMesh/from/" + sFrom.substring(sFrom.length()-3);
+  mqttClient.publish(topic.c_str(), msg.c_str());
+}
+
+void mqttCallback(char* topic, uint8_t* payload, unsigned int length) {
+  char* cleanPayload = (char*)malloc(length+1);
+  payload[length] = '\0';
+  memcpy(cleanPayload, payload, length+1);
+  String msg = String(cleanPayload);
+  free(cleanPayload);
+
+  String targetStr = String(topic).substring(16);
+
+  if(targetStr == "gateway")
+  {
+    if(msg == "getNodes")
+    {
+      mqttClient.publish("MyMesh/from/gateway", mesh.subConnectionJson().c_str());
+    }
+  }
+  else if(targetStr == "broadcast")
+  {
+    mesh.sendBroadcast(msg);
+  }
+  else
+  {
+    uint32_t target = strtoul(targetStr.c_str(), NULL, 10);
+    if(mesh.isConnected(target))
+    {
+      mesh.sendSingle(target, msg);
+    }
+    else
+    {
+      mqttClient.publish("MyMesh/from/gateway", "Client not connected!");
+    }
+  }
+}
+
+IPAddress getlocalIP() {
+  return IPAddress(mesh.getStationIP());
+}
